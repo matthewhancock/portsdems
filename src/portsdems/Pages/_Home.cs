387f83a90701871267fc853696022defba0f11e3
Matthew Hancock <git@mt.hn>
2015-07-20 18:00:48 -0400

    Update _Home.cs
    Overwrote current events, clearing old information and will update shortly.

--- src/portsdems/Pages/_Home.cs
+++ src/portsdems/Pages/_Home.cs
@@ -4,7 +4,7 @@ namespace portsmouth_democrats.Pages {
 	public class Home : Site.Page {
 
 		public static string OutputPage() {
-			return "<div class=\"tac\"><h2>Upcoming Events</h2><h3>Thank You Dinner for Terie Norelli</h3><div class=\"b\">Saturday, March 21<br />6PM<br /><a href=\"http://binged.it/1M3LMoV\" target=\"_blank\">Portsmouth Harbor Events and Conference Center</a></div><br />The Portsmouth Democratic Committee will hold a dinner the evening of Saturday, March 21st to thank Terie Norelli for all her years of service in the legislature. A number of political and community leaders who know Terie personally will be featured. The event is a fundraiser for the Portsmouth Democratic Committee, to help us get prepared for the big 2016 elections.<br /><br />The dinner will be held at the Portsmouth Harbor Events and Conference Center in downtown Portsmouth.<br /><br />Doors open at 6:00 pm for a cash bar and dinner will be served at 7:00 pm.<br /><br />It’s first come - first serve and tickets are going fast. We anticipate a sell-out. Here’s a link to the Act Blue page where you can buy your ticket online: <a href=\"https://secure.actblue.com/contribute/page/terie-dinner\" target=\"_blank\">https://secure.actblue.com/contribute/page/terie-dinner</a>";
+			return "<div class=\"tac\"><h2>Upcoming Events</h2></div>";
 		}
 
 		public override Func<string> Content {
