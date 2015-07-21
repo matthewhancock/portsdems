2a36e2ee55f8f253cc31e2093f5fd5ff93a32eea
Matthew Hancock <git@mt.hn>
2015-07-20 20:53:56 -0400

    Update _Home.cs
    testing automatic deployment to azure

--- src/portsdems/Pages/_Home.cs
+++ src/portsdems/Pages/_Home.cs
@@ -2,7 +2,6 @@
 
 namespace portsmouth_democrats.Pages {
 	public class Home : Site.Page {
-
 		public static string OutputPage() {
 			return "<div class=\"tac\"><h2>Upcoming Events</h2><h3>Portsmouth Democrats Monthly Roundtable</h3><div class=\"b\">Tuesday, July 21, 2015<br />6:30PM<br />Cafe Nostimo<br /><a href=\"http://binged.it/1cFa7EC\" target=\"_blank\">72 Mirona Road</a></div><br />All Democrats are welcome. This is a social gathering with no agenda or speakers, just good food, good conversation, with good people.";
 		}
